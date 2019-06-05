07609034b1055c701edeb52350be5899c0cbeadc
KrystalCW <krystal.wiederrich@gmail.com>
2019-06-05 10:58:04 -0700

    Updates

---
+++ precache-manifest.00c724d447903b30a93180b150c14c0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a84c7f77d0b6303418d6c5f2d8b765",
+    "url": "/Doggo-Memory-Game/index.html"
+  },
+  {
+    "revision": "738794de42fbce3461c6",
+    "url": "/Doggo-Memory-Game/static/css/main.3f2ce128.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Doggo-Memory-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "738794de42fbce3461c6",
+    "url": "/Doggo-Memory-Game/static/js/main.e41c8a01.chunk.js"
+  },
+  {
+    "revision": "31eddc98782f9537611f",
+    "url": "/Doggo-Memory-Game/static/js/runtime~main.c24fd6e6.js"
+  }
+]);
